72dd2e82bbd5d6b5da48c99667a61d773a08cdf3
Jaeyoung Lim <eia51@naver.com>
2021-11-01 20:54:33 +0900

    백준 그리디 19539 사과나무 풀이

---
+++ Algorithm/boj/greedy/19539.py
@@ -0,0 +1,18 @@
+# ref -> https://www.acmicpc.net/problem/19539
+import sys
+read = sys.stdin.readline
+
+N = int(read())
+apple = list(map(int, read().split()))
+apple_sum = sum(apple)
+turn = apple_sum // 3
+
+if apple_sum % 3 != 0:
+    print('NO')
+else:
+    for a in apple:
+        turn -= (a//2)
+    if turn > 0:
+        print('NO')
+    else:
+        print('YES')
