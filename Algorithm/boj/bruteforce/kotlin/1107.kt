5510b7662bebeacc43d9dbd9f779587fc2e8c66b
Jaeyoung Lim <eia51@naver.com>
2021-11-11 14:54:12 +0900

    백준 브루트포스 1107 리모컨 풀이
    아래 테스트 케이스에 대한 고려가 부족했음
    104
    7
    0 1 2 3 4 5 9

---
+++ Algorithm/boj/bruteforce/kotlin/1107.kt
@@ -0,0 +1,69 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.*
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+
+
+val CURRENT = 100
+val KEYS = IntArray(10) { 1 }
+
+// ref -> https://www.acmicpc.net/problem/1107
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val channelString = readLine()
+    val channel = channelString.toInt()
+    val n = readLine().toInt()
+    if (n > 0) {
+        val token = StringTokenizer(readLine(), " ")
+        for (i in 0 until n)
+            KEYS[token.nextToken().toInt()] = 0
+    }
+
+    if (channel == CURRENT) {
+        println(0)
+        return
+    }
+
+    val gap = abs(CURRENT - channel)
+    if (gap <= channelString.length || n == 10) {
+        println(gap)
+        return
+    }
+
+    if (isAvailable(channel)) {
+        println(channelString.length)
+        return
+    }
+
+    var offset = 1
+    while (true) {
+        val less = max(channel - offset, 0)
+        if (isAvailable(less)) {
+            printCount(less, channel, gap)
+            break
+        }
+        val more = channel + offset
+        if (isAvailable(more)) {
+            printCount(more, channel, gap)
+            break
+        }
+        offset++
+    }
+}
+
+fun isAvailable(channel: Int): Boolean {
+    val str = channel.toString()
+    for (i in str) {
+        if (KEYS[i.code - 48] != 1) {
+            return false
+        }
+    }
+    return true
+}
+
+fun printCount(target: Int, origin: Int, gap: Int) {
+    val targetStr = target.toString()
+    val count = min(targetStr.length + abs(target - origin), gap)
+    println(count)
+}
