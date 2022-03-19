c04bce661193e63f03e5ee71f0b3de30f796ea7e
JAEYOUNG LIM <jyl@JAEYOUNGui-MacBookPro.local>
2022-03-19 22:56:16 +0900

    저장소 리포맷팅

--- Algorithm/boj/math/1676.py
+++
@@ -1,8 +0,0 @@
-from math import factorial
-n = int(input())
-cnt = 0
-for x in str(factorial(n))[::-1]:
-    if x != '0':
-        break
-    cnt += 1
-print(cnt)
--- Algorithm/boj/tree/9372.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-for t in range(int(input())):
-    N, M = map(int, input().split())
-    for i in range(M):
-        a, b = map(int, sys.stdin.readline().split())
-
-    print(N-1)
